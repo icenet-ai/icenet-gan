20dd3c4ceb7fba2061528e5936f8ad2364160a4b
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2024-08-22 13:48:27 +0100

    Start adding losses from draft GAN + icenet notebook
    Ref: https://github.com/icenet-ai/icenet-notebooks/blob/66d219ce7be76d67da01cec627d7c6be1a33c2d9/pytorch/1_icenet_forecast_unet.ipynb

---
+++ src/icenet_gan/models/losses.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+
+
+class WeightedBCEWithLogitsLoss(nn.BCEWithLogitsLoss):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, inputs, targets, sample_weights):
+        """
+        Weighted BCEWithLogitsLoss loss.
+
+        Compute BCEWithLogitsLoss loss weighted by masking.
+
+        Using BCEWithLogitsLoss instead of BCELoss, as pytorch docs mentions it is
+        more numerically stable.
+        https://pytorch.org/docs/stable/generated/torch.nn.BCEWithLogitsLoss.html
+        
+        """
+        # Computing using nn.BCEWithLogitsLoss base class. This class must be instantiated via:
+        # >>> criterion = WeightedBCEWithLogitsLoss(reduction='none')
+        loss = super().forward(
+                            (inputs.movedim(-2, 1)),
+                            (targets.movedim(-1, 1))
+                         )*sample_weights.movedim(-1, 1)
+
+        return loss.mean()
+
+
+class WeightedL1Loss(nn.L1Loss):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, inputs, targets, sample_weights):
+        """
+        Weighted L1 loss.
+
+        Compute L1 loss weighted by masking.
+        
+        """
+        y_hat = torch.sigmoid(inputs)
+
+        # Computing using nn.L1Loss class. This class must be instantiated via:
+        # >>> criterion = WeightedL1Loss(reduction="none")
+        loss = super().forward(
+                            (100*y_hat.movedim(-2, 1)), 
+                            (100*targets.movedim(-1, 1))
+                         )*sample_weights.movedim(-1, 1)
+
+
+        # Computing here, in the derived class
+        # loss = (
+        #             torch.abs( ( y_hat.movedim(-2, 1) - targets.movedim(-1, 1) )*100 )
+        #         )*sample_weights.movedim(-1, 1)
+        
+        return loss.mean()
+
+
+class WeightedMSELoss(nn.MSELoss):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, inputs, targets, sample_weights):
+        """
+        Weighted MSE loss.
+
+        Compute MSE loss weighted by masking.
+        
+        """
+        y_hat = torch.sigmoid(inputs)
+
+        # Computing using nn.MSELoss base class. This class must be instantiated via:
+        # criterion = nn.MSELoss(reduction="none")
+        loss = super().forward(
+                            (100*y_hat.movedim(-2, 1)), 
+                            (100*targets.movedim(-1, 1))
+                         )*sample_weights.movedim(-1, 1)
+
+
+        # Computing here, in the nn.Module derived class
+        # loss = (
+        #             ( ( y_hat.movedim(-2, 1) - targets.movedim(-1, 1) )*100 )**2
+        #         )*sample_weights.movedim(-1, 1)
+        return loss.mean()
