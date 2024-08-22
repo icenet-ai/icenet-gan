497218275bfbcc6f65e16b650d67379f59ed11ca
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2024-08-22 13:54:43 +0100

    Start adding UNet model wrapper from draft GAN + icenet notebook
    Ref: https://github.com/icenet-ai/icenet-notebooks/blob/66d219ce7be76d67da01cec627d7c6be1a33c2d9/pytorch/1_icenet_forecast_unet.ipynb

---
+++ src/icenet_gan/models/model_wrapper.py
@@ -0,0 +1,195 @@
+import torch
+import torch.nn as nn
+import lightning.pytorch as pl
+
+from lightning.pytorch.utilities.types import TRAIN_DATALOADERS
+from torchmetrics import MetricCollection
+
+from .metrics import IceNetAccuracy, SIEError
+
+
+class LitUNet(pl.LightningModule):
+    """
+    A LightningModule wrapping the UNet implementation of IceNet.
+    """
+    def __init__(self,
+                 model: nn.Module,
+                 criterion: callable,
+                 learning_rate: float):
+        """
+        Construct a UNet LightningModule.
+        Note that we keep hyperparameters separate from dataloaders to prevent data leakage at test time.
+        :param model: PyTorch model
+        :param criterion: PyTorch loss function for training instantiated with reduction="none"
+        :param learning_rate: Float learning rate for our optimiser
+        """
+        super().__init__()
+        self.model = model
+        self.criterion = criterion
+        self.learning_rate = learning_rate
+        self.n_output_classes = model.n_output_classes  # this should be a property of the network
+
+        self.metrics_history = {
+            "train_loss": [],
+            "val_loss": [],
+            "val_accuracy": [],
+        }
+
+        self.training_step_outputs = []
+        self.validation_step_outputs = []
+
+        # Overall accuracy
+        metrics = {
+            "val_accuracy": IceNetAccuracy(leadtimes_to_evaluate=list(range(self.model.n_forecast_days))),
+            "val_sieerror": SIEError(leadtimes_to_evaluate=list(range(self.model.n_forecast_days)))
+        }
+
+        # Accuracy over leadtime
+        for i in range(self.model.n_forecast_days):
+            metrics[f"val_accuracy_{i}"] = IceNetAccuracy(leadtimes_to_evaluate=[i])
+            metrics[f"val_sieerror_{i}"] = SIEError(leadtimes_to_evaluate=[i])
+        self.metrics = MetricCollection(metrics)
+
+        # Overall accuracy
+        test_metrics = {
+            "test_accuracy": IceNetAccuracy(leadtimes_to_evaluate=list(range(self.model.n_forecast_days))),
+            "test_sieerror": SIEError(leadtimes_to_evaluate=list(range(self.model.n_forecast_days)))
+        }
+
+        # Accuracy over leadtime
+        for i in range(self.model.n_forecast_days):
+            test_metrics[f"test_accuracy_{i}"] = IceNetAccuracy(leadtimes_to_evaluate=[i])
+            test_metrics[f"test_sieerror_{i}"] = SIEError(leadtimes_to_evaluate=[i])
+        self.test_metrics = MetricCollection(test_metrics)
+
+        # Save input parameters to __init__ (hyperparams) when checkpointing.
+        # self.save_hyperparameters(ignore=["model", "criterion"])
+        self.save_hyperparameters()
+
+
+    def forward(self, x):
+        """
+        Implement forward function.
+        :param x: Inputs to model.
+        :return: Outputs of model.
+        """
+        return self.model(x)
+
+
+    def training_step(self, batch):
+        """
+        Perform a pass through a batch of training data.
+        Apply pixel-weighted loss by manually reducing.
+        See e.g. https://discuss.pytorch.org/t/unet-pixel-wise-weighted-loss-function/46689/5.
+        :param batch: Batch of input, output, weight triplets
+        :param batch_idx: Index of batch
+        :return: Loss from this batch of data for use in backprop
+        """
+        x, y, sample_weight = batch
+        outputs = self.model(x)
+        # y_hat = torch.sigmoid(outputs)
+
+        # loss = loss_func(outputs, y, sample_weight)
+        loss = self.criterion(outputs, y, sample_weight)
+        
+        self.training_step_outputs.append(loss)
+
+        # self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, sync_dist=True)
+        return {"loss": loss}
+
+
+    def validation_step(self, batch):
+        # x: (b, h, w, channels), y: (b, h, w, n_forecast_days, classes), sample_weight: (b, h, w, n_forecast_days, classes)
+        x, y, sample_weight = batch
+        outputs = self.model(x)
+
+        # y_hat: (b, h, w, classes, n_forecast_days)
+        y_hat = torch.sigmoid(outputs)
+
+        # loss = loss_func(outputs, y, sample_weight)
+        loss = self.criterion(outputs, y, sample_weight)
+        self.validation_step_outputs.append(loss)
+
+        # self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)  # epoch-level loss
+
+        self.metrics.update(y_hat.squeeze(dim=-2), y.squeeze(dim=-1), sample_weight.squeeze(dim=-1))
+
+        return {"val_loss", loss}
+
+
+    # def training_epoch_end(self, outputs):
+    #     avg_train_loss = torch.stack([x["train_loss"] for x in outputs]).mean()
+    #     self.metrics_history["train_loss"].append(avg_train_loss.item())
+
+
+    # def validation_epoch_end(self, outputs):
+    #     avg_val_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+    #     avg_val_accuracy = torch.stack([x["val_accuracy"] for x in outputs]).mean()
+    #     self.metrics_history["val_loss"].append(avg_train_loss.item())
+    #     self.metrics_history["val_accuracy"].append(avg_train_accuracy.item())
+
+
+    def on_train_epoch_end(self):
+        """
+        Reference lightning v2.0.0 migration guide:
+        https://github.com/Lightning-AI/pytorch-lightning/pull/16520
+        """
+        epoch_average = torch.stack(self.training_step_outputs).mean()
+        self.log("train_loss", epoch_average)#, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.training_step_outputs.clear()  # free memory
+        self.metrics_history["train_loss"].append(epoch_average.item())
+
+
+    def on_validation_epoch_end(self):
+        """
+        Reference lightning v2.0.0 migration guide:
+        https://github.com/Lightning-AI/pytorch-lightning/pull/16520
+        """
+        epoch_average = torch.stack(self.validation_step_outputs).mean()
+        self.log("validation_loss", epoch_average)#, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.validation_step_outputs.clear()  # free memory
+        self.log_dict(self.metrics.compute(), on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)  # epoch-level metrics
+        print(self.metrics["val_accuracy"])
+        self.metrics.reset()
+        self.metrics_history["val_loss"].append(epoch_average.item())
+
+
+    def test_step(self, batch):
+        x, y, sample_weight = batch
+        outputs = self.model(x)
+        y_hat = torch.sigmoid(outputs)
+
+        # loss = loss_func(outputs, y, sample_weight)
+        loss = self.criterion(outputs, y, sample_weight)
+
+        self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)  # epoch-level loss
+
+        self.test_metrics.update(y_hat.squeeze(dim=-2), y.squeeze(dim=-1), sample_weight.squeeze(dim=-1))
+    
+        return loss
+
+
+    def on_test_epoch_end(self):
+        self.log_dict(self.test_metrics.compute(), on_step=False, on_epoch=True, sync_dist=True)  # epoch-level metrics
+        self.test_metrics.reset()
+
+
+    def predict_step(self, batch):
+        """
+        :param batch: Batch of input, output, weight triplets
+        :param batch_idx: Index of batch
+        :return: Predictions for given input.
+        """
+        x, y, sample_weight = batch
+        y_hat = torch.sigmoid(self.model(x))
+
+        return y_hat
+
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return {
+            "optimizer": optimizer
+        }
+
+
