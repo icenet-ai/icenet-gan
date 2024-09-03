dba7c17c10aff3cad585b4fb20c003ca1386a6e7
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2024-09-03 01:10:33 +0100

    Add MAE and RMSE metrics

--- src/icenet_gan/models/metrics.py
+++ src/icenet_gan/models/metrics.py
@@ -1,6 +1,9 @@
 import torch
+import torchmetrics
 
 from torchmetrics import Metric
+from torchmetrics.functional.regression.mae import _mean_absolute_error_update
+from torchmetrics.functional.regression.mse import _mean_squared_error_update
 
 
 class IceNetAccuracy(Metric):
@@ -71,3 +74,64 @@ class SIEError(Metric):
 
     def compute(self):
         return (self.pred_sie - self.true_sie) * 25**2 # each pixel is 25x25 km
+
+
+class WeightedMAE(torchmetrics.MeanAbsoluteError):
+    """MAE metric for use at multiple leadtimes.
+
+    Reference: https://lightning.ai/docs/torchmetrics/stable/pages/implement.html
+    """
+
+    def __init__(self, leadtimes_to_evaluate: list = None):
+        """Weighted MAE metric for multiple leadtimes.
+
+        Args:
+            leadtimes_to_evaluate: A list of leadtimes to consider
+                e.g., [0, 1, 2, 3, 4, 5] to consider first six days in accuracy computation or
+                e.g., [0] to only look at the first day's accuracy
+                e.g., [5] to only look at the sixth day's accuracy
+        """
+        # Pass `squared=False` to get RMSE instead of MSE
+        super().__init__()
+        self.leadtimes_to_evaluate = leadtimes_to_evaluate if leadtimes_to_evaluate is not None else slice(None)
+
+    def update(self, preds, target, sample_weight: torch.Tensor) -> None:
+        """Update state with predictions and targets."""
+        predictions = (preds*sample_weight)[..., self.leadtimes_to_evaluate]
+        targets = (target*sample_weight)[..., self.leadtimes_to_evaluate]
+        sum_abs_error, num_obs = _mean_absolute_error_update(predictions, targets)
+
+        self.sum_abs_error += sum_abs_error
+        self.total += num_obs
+
+
+class WeightedRMSE(torchmetrics.MeanSquaredError):
+    """RMSE metric for use at multiple leadtimes.
+
+    Reference: https://lightning.ai/docs/torchmetrics/stable/pages/implement.html
+    """
+
+    def __init__(self, leadtimes_to_evaluate: list = None):
+        """Weighted RMSE metric for multiple leadtimes.
+
+        Args:
+            leadtimes_to_evaluate: A list of leadtimes to consider
+                e.g., [0, 1, 2, 3, 4, 5] to consider first six days in accuracy computation or
+                e.g., [0] to only look at the first day's accuracy
+                e.g., [5] to only look at the sixth day's accuracy
+        """
+        # Pass `squared=False` to get RMSE instead of MSE
+        super().__init__(squared=False)
+        self.leadtimes_to_evaluate = leadtimes_to_evaluate if leadtimes_to_evaluate is not None else slice(None)
+
+    def update(self, preds, target, sample_weight: torch.Tensor) -> None:
+        """Update state with predictions and targets."""
+        predictions = (preds*sample_weight)[..., self.leadtimes_to_evaluate]
+        targets = (target*sample_weight)[..., self.leadtimes_to_evaluate]
+        sum_squared_error, num_obs = _mean_squared_error_update(predictions,
+                                                                targets,
+                                                                num_outputs=self.num_outputs
+                                                                )
+
+        self.sum_squared_error += sum_squared_error
+        self.total += num_obs
